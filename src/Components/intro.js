66e1ccd1f65c7b5507d9b6fd2881a0fcf6f1b944
RajuPerumal <rajusekarpsr@gmail.com>
2024-08-22 19:57:38 +0530

    resume updated

--- src/Components/intro.js
+++ src/Components/intro.js
@@ -1,33 +1,42 @@
-import React from 'react'
-import './intro.css'
-// import {Link} from "react-scroll"
-import HireMe from "../assets/HireMe.png"
-import ProfilePic from "../assets/png 3.png"
-// import {handleDownload} from "./Constants.js"
-// import TextRevealing from "./TextRevealing"
+import React from 'react';
+import './intro.css';
+import HireMe from '../assets/HireMe.png';
+import ProfilePic from '../assets/png 3.png';
 
+const Intro = () => {
 
-const Intro = ()=> {
+  // Function to handle PDF download
+  const handleDownload = () => {
+    const pdfUrl = "Raju_Resume_Updated_On_2024.pdf"; // Replace with your cloud storage URL
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "Rajasekar_Resume.pdf"; // The name you want the downloaded file to have
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <section id="intro">
-        <div className="introContent">
-            <span className="hello">Hello,</span>
-            <span className="IntroText">I'm 
-            <span className="IntroName"> Rajasekar</span>
-            <br /><span className='frontenddev'>Front End Developer</span>
-            </span>
-            <p className="IntroPara">Empowering Lives through Code, Compassion, and Inclusivity</p>
-            <div className='hiremeLink'>
-              <img src={HireMe} alt='hireme'className='btnImg'/>
-              <button className='HireMeButton' 
-              // onClick={handleDownload}
-              >Hire Me</button>
-            </div>
-            {/* <Link ><button className="hiremebtn"><img src={HireMe} alt="hireme" className='btnImg'/>Hire Me</button></Link> */}
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="IntroText">
+          I'm 
+          <span className="IntroName"> Rajasekar</span>
+          <br />
+          <span className='frontenddev'>Front End Developer</span>
+        </span>
+        <p className="IntroPara">Empowering Lives through Code, Compassion, and Inclusivity</p>
+        <div className='hiremeLink'>
+          <img src={HireMe} alt='hireme' className='btnImg' />
+          <button className='HireMeButton' onClick={handleDownload}>
+            Hire Me
+          </button>
         </div>
-        <img src={ProfilePic} alt="profile" className="bg" />
+      </div>
+      <img src={ProfilePic} alt="profile" className="bg" />
     </section>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
